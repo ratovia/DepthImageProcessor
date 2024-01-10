0bd8b1dc84d02e765d83de2d92da8f117091bec5
ratovia <ratovia@hotmail.co.jp>
2024-01-10 14:20:11 +0900

    グラフ化

--- DepthImageProcessor/main.py
+++ DepthImageProcessor/main.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 import numpy as np
-
+import matplotlib.pyplot as plt
 class DepthImageProcessor(Node):
     def __init__(self, camera_name):
         super().__init__('depth_image_processor')
@@ -20,13 +20,20 @@ class DepthImageProcessor(Node):
         width = depth_msg.width
         np_data = np.frombuffer(raw_data, dtype=np.uint16).reshape(height, width)
 
+        # ロギングのための条件付きチェック
         for y in range(height):
             for x in range(width):
-                if x % 100 != 0 or y % 100 != 0:
-                    continue
+                if x % 100 == 0 and y % 100 == 0:
+                    self.logger.info(f"y:{y} x:{x} {np_data[y, x]} mm")
 
-                distance = np_data[y, x]
-                self.logger.info(f"y:{y} x:{x} {distance} mm")
+        # 2次元データをプロット
+        plt.imshow(np_data, cmap='hot', interpolation='nearest')
+        plt.colorbar(label='Distance (mm)')
+        plt.xlabel('X Pixel')
+        plt.ylabel('Y Pixel')
+        plt.title('Depth Image Distance Data')
+        plt.show()
+
 
 def main(args=None):
     rclpy.init(args=args)
